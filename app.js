bc248cc4372f7429b0d911b4e62a3513cdb0fcca
suminChoi <suminc255@gmail.com>
2024-03-10 23:44:23 +0900

    create blend page

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const mysql = require("./routes/mysqlconn");
-// 로그인
 const session = require("express-session");
-//
+
 const app = express();
 
 mysql.connect();
@@ -13,6 +12,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: false }));
+
 app.use(
   session({
     name: "user",
@@ -22,13 +22,25 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.locals.user_id = "";
+  if (req.session.user) {
+    res.locals.user_id = req.session.user.user_id;
+  }
+  next();
+});
+
 const landingRouter = require("./routes/langding");
 const signinRouter = require("./routes/signin");
 const signupRouter = require("./routes/signup");
+const logoutRouter = require("./routes/logout");
+const blendRouter = require("./routes/blend");
 
 app.use("/", landingRouter);
 app.use("/signin", signinRouter);
 app.use("/signup", signupRouter);
+app.use("/logout", logoutRouter);
+app.use("/blend", blendRouter);
 
 app.listen(3000, function () {
   console.log("Server Running at http://127.0.0.1:3000");
